8ccde111e1744c5392057f82d636f1f2c97e8cb1
USER-PC\USER\Documents <j35148@gmail.com>
2019-06-23 23:02:31 +0900

    네이버 유효성 스크립트 추가

--- 2.HTML&CSS&JS/project/naver/js/script.js
+++ 2.HTML&CSS&JS/project/naver/js/script.js
@@ -1,54 +1,180 @@
 $(document).ready(function(){
 
     //            var inputBox = $('.input_box');
-    var select = $('select');
-    var input = $('input');
+    
     //            var birthList = $('birth_list'); 
     
     
     // p태그 넣을 div
     var signupList = $('.signup_list');
+    var form = $('#register_form');
 
-
-    // focusin되면 bordercolor - green
-    function setBorderGreen(e){
-        $(e.target).parent().css('border-color','#09a600');
-    }
-
-    // focusout되면 bordercolor - gray
-    function setBorderDefault(e){
-        $(e.target).parent().css('border-color','#bbb');
-    }
-
-
+    
     // focusin / focusout 이벤트 처리
-    input.on('focusin',function(e){
-        setBorderGreen(e);
+    $('input, select').on('focusin',function(e){
+        $(e.target).parent().css('border-color','#09a600');
+        
+        return false;
     });
-    select.on('focusin',function(e){
-        setBorderGreen(e);
-    })
 
-    input.on('focusout',function(e){
-        setBorderDefault(e);  
-        showMsg(e);
-    });
-    select.on('focusout',function(e){
-        setBorderDefault(e); 
-        showMsg(e);
+    $('input, select').on('focusout',function(e){
+        $(e.target).parent().css('border-color','#bbb'); 
+//        showMsg(e);
+        
+        return false;
     });
 
 
+    
+    // 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.
+        var idCheck = /^[a-z0-9-_]{5,20}$/; 
+        //8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
+        var pwCheck = /^[A-Za-z0-9가-힣]{8,16}$/;
+        // 한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)
+        var nameCheck = /^[가-힣A-Z]{0,30}$/;
+        
+        // 1900~2019 / 4자리 이상
+        var yearCheck = /^(19[0-9][0-9]|20[0-1][0-9])$/;
 
-    // 필수 사항 메시지 뜨게 하는 함수
-    function showMsg(e){
-        console.log(e.target);
-        if(e.target.value < 1){
-            console.log(e.target.closest('.signup_list')); 
-            e.target.closest('.signup_list').append($('p'));
+        // 2자리 이상
+        var dayCheck = /^(0*[0-9]|[1-2][0-9]|[3][0-1])$/;
+        
+        // @ / .com / .net / .co.kr
+        var emailCheck = /^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/;
+        
+        // 010 시작
+        var pNumCheck = /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/;
+        
+        
+        $('#signup_id').on('focusout',function(e){
+            
+            if($(this).val().length > 0){
+                
+                if(!idCheck.test($(this).val())){
+                $('#id_error_msg').html('5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.').css('color','red');
+                } else {
+                    $('#id_error_msg').html('멋진 아이디네요!').css('color','green');
+                }
+            } else{
+                $('#id_error_msg').html('필수 사항입니다.').css('color','red');
+            }
+            
+            return false;
+        });
+    
+        $('#signup_pw').on('focusout',function(e){
+            
+            if($(this).val().length > 0){
+                
+                if(!pwCheck.test($(this).val())){
+                    // 사용불가 메시지 띄우기
+                    $('#pw_error_msg').html('8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.').css('color','red');
+                } else {
+                    // 안전 마크 바꾸기.
+                }
+            } else{
+                $('#pw_error_msg').html('필수 사항입니다.').css('color','red');
+            }
+            
+            return false;
+        });
+    
+        $('#signup_repw').on('focusout',function(){
+           if($(this).val() != $('#signup_pw').val()){
+               $('#repw_error_msg').html('비밀번호가 일치하지 않습니다.').css('color','red');
+           } else{
+                $('#repw_error_msg').html('필수 사항입니다.').css('color','red');
+            }
+            return false;
+        });
+    
+        $('#signup_name').on('focusout',function(e){
+            
+            if($(this).val().length > 0){
+                
+                if(!nameCheck.test($(this).val())){
+                    // 사용불가 메시지 띄우기
+                    $('#name_error_msg').html('한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)').css('color','red');
+                } else {
+                    $('#name_error_msg').html('');
+                }
+            } else{
+                $('#name_error_msg').html('필수 사항입니다.').css('color','red');
+            }
+            
+            return false;
+        });
+    
+        $('#signup_year').on('focusout',function(e){
+            
+            if($(this).val().length > 0){
+                
+                if(!yearCheck.test($(this).val())){
+                    // 사용불가 메시지 띄우기
+                    $('#birth_error_msg').html('태어난 년도 4자리를 정확하게 입력하세요.').css('color','red');
+                } else {
+                    $('#birth_error_msg').html('');
+                }
+            } else{
+                $('#birth_error_msg').html('필수 사항입니다.').css('color','red');
+            }
+            
+            return false;
+        });
+    
+    $('#signup_day').on('focusout',function(e){
+            
+            if($(this).val().length > 0){
+                
+                if(!dayCheck.test($(this).val())){
+                    // 사용불가 메시지 띄우기
+                    $('#birth_error_msg').html('태어난 일 2자를 정확하게 입력하세요.').css('color','red');
+                } else {
+                    $('#birth_error_msg').html('');
+                }
+            } else{
+                $('#birth_error_msg').html('필수 사항입니다.').css('color','red');
+            }
+            
+            return false;
+    });
+    
+    $('#signup_email').on('focusout',function(e){
+            
+            if($(this).val().length > 0){
+                
+                if(!emailCheck.test($(this).val())){
+                    // 사용불가 메시지 띄우기
+                    $('#email_error_msg').html('이메일 주소를 다시 확인해주세요.').css('color','red');
+                } else {
+                    $('#email_error_msg').html('');
+                }
+            }
+            
+            return false;
+    });
+    
+    $('#signup_number').on('focusout',function(e){
+            
+            if($(this).val().length > 0){
+                
+                if(!pNumCheck.test($(this).val())){
+                    // 사용불가 메시지 띄우기
+                    $('#num_error_msg').html('이메일 주소를 다시 확인해주세요.').css('color','red');
+                } else {
+                    $('#num_error_msg').html('');
+                }
+            } else{
+                $('#num_error_msg').html('필수 사항입니다.').css('color','red');
+            }
+            
+            return false;
+    });
+    
+    form.on('submit',function(){
 
-        }
-    }
+    });
+    
 
 });
 
