c2f9d1e7b8460b25a66adf594b83be93a5ed0bef
orongee22 <j35148@gmail.com>
2019-07-12 18:08:44 +0900

    jsp - filter 수업내용

---
+++ 3.JSP/JSPStuding/WebContent/member/form.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page trimDirectiveWhitespaces="true" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script
+  src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<style>
+</style>
+</head>
+<body>
+	<h1>회원가입</h1>
+	<form action="reg.jsp" method="post">
+		이름<input type="text" name="name"><input type="submit" value="가입">
+	</form>
+</body>
+</html>
---
+++ 3.JSP/JSPStuding/src/filter/LoginCheckFilter.java
@@ -0,0 +1,53 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebFilter("/mypage/*")
+public class LoginCheckFilter implements Filter {
+
+    public LoginCheckFilter() {
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse  resp = (HttpServletResponse)response;
+		HttpSession session = req.getSession(false);
+		
+		boolean loginChk = false;
+		
+		if(session !=null & session.getAttribute("LoginInfo") !=null){
+			loginChk = true;
+		}
+		
+		if(loginChk) {
+			chain.doFilter(request, response);
+		} else {
+			resp.sendRedirect(req.getContextPath()+"/session/member/loginForm.jsp");
+			
+			
+		}
+		
+		
+		
+		
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
